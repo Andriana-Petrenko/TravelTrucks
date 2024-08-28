99833dc1ea62babf9893bc0c252628d39da2db1a
Andriana-Petrenko <zlata0707petrenko@gmail.com>
2024-08-28 14:06:54 +0300

    add pages

---
+++ src/pages/CatalogPage/CatalogPage.jsx
@@ -0,0 +1,14 @@
+import { Helmet } from "react-helmet-async"
+
+const CatalogPage = () => {
+  return (
+    <>
+        <Helmet>
+            <title>Catalog Page</title>
+        </Helmet>
+        <div>CatalogPage</div>
+    </>
+  )
+}
+
+export default CatalogPage
---
+++ src/pages/DetailsPage/DetailsPage.jsx
@@ -0,0 +1,12 @@
+import { Helmet } from "react-helmet-async"
+
+export const DetailsPage = () => {
+    return (
+     <>
+          <Helmet>
+                <title>Details Page</title>
+            </Helmet>
+        <div>DetailsPage</div>
+     </>
+  )
+}
